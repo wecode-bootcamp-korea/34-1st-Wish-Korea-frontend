0b056bf962c04f3a6f6d4a3f9e6c3b67d55d2389
Sangwon Ha <305243@naver.com>
2022-06-20 19:53:15 +0900

    초기 PR 작성

--- src/pages/ProductView/ProductView.js
+++ src/pages/ProductView/ProductView.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const ProductView = () => {
-  return <div>ProductView</div>;
+  return (
+    <div>
+      ProductView 상세 목데이터 구성 데이터 불러오기 공유하기 링크 복사
+      장바구니로 데이터 보내기 옵션 체크 카테고리 이동 박스
+      상세정보/상품후기/배송교환 이동 페이지
+    </div>
+  );
 };
 
 export default ProductView;
