d90d547cc7ddd5dc7934fcf87e5d610e05eac99d
Ahnjungyoung <90163269+Ahnjungyoung@users.noreply.github.com>
2022-06-21 16:02:45 +0900

    Add:로그인 레이아웃 작업중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import './Login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="header">
+        <h3>로그인</h3>
+      </div>
+      <div className="membership">
+        <p className="member">회원</p>
+        <p className="nonMember">비회원</p>
+      </div>
+      <div className="loginInput">
+        <form>
+          <p>
+            <input className="identification" />
+          </p>
+          <p>
+            <input className="password" />
+          </p>
+          <p>
+            <button className="identificationSave" />
+          </p>
+          <p>
+            <input type="button" className="loginButton" />
+          </p>
+          <footer>
+            <p>회원가입 | 아이디 찾기 | 비밀번호 찾기 |</p>
+          </footer>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
