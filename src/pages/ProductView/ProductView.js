d29723928aadedf638358d8dc66477145854b28e
Sangwon Ha <305243@naver.com>
2022-06-22 19:09:16 +0900

    상세 옵션클릭 기능 추가

--- src/pages/ProductView/ProductView.js
+++ src/pages/ProductView/ProductView.js
@@ -1,9 +1,14 @@
 import { React, useState, useEffect } from 'react';
 import './ProductView.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const ProductView = () => {
   const [viewData, setViewData] = useState([]);
-  const [price, setPrice] = useState('');
+  const [knowClick, setKnowClick] = useState(false);
+  const [clickOptions, setClickOptions] = useState(false);
+  const [totalThing, setTotalThing] = useState([]);
+  let tPrice = 0;
 
   useEffect(() => {
     fetch('./data/View.json', { method: 'GET' })
@@ -13,21 +18,69 @@ const ProductView = () => {
       });
   }, []);
 
+  const totalMap = totalThing.map(el => {
+    return (
+      <div key={el.oid}>
+        수량체크 {el.weight}/{el.price} X버튼
+      </div>
+    );
+  });
+  const handleOptions = el => {
+    const selectArr = [];
+    totalThing.map(el => {
+      selectArr.push(el.oid);
+    });
+    !selectArr.includes(el.oid)
+      ? setTotalThing([...totalThing, el])
+      : alert('안돼!');
+  };
+
+  totalThing?.map(el => {
+    tPrice = tPrice + el.price;
+  });
+
+  console.log(totalThing);
   return (
     <div className="productView">
       <div className="detailInfo">
-        <img className="detailImg" src="images/viewImg.png" alt="상세이미지" />
+        <img className="detailImg" src={viewData.img} alt="상세이미지" />
         <div className="detailBox">
           <span className="detailName">
             {viewData.name}
-            {'   '} {viewData.option}
+            {'   '}
+            {viewData.option?.map((el, idx) => {
+              return idx === 0 ? (
+                <span key={el.oid}>{el.weight}</span>
+              ) : (
+                <span key={el.oid}>/{el.weight}</span>
+              );
+            })}
             <img className="detailShare" src="images/share.png" alt="share" />
           </span>
           <div className="detailKeyword">{viewData.keyword}</div>
           <div className="detailText">
             Good to know
             {'  '}
-            <img className="know" src="images/know.png" alt="know" />
+            <img
+              className="know"
+              src="images/know.png"
+              alt="know"
+              onClick={() =>
+                knowClick ? setKnowClick(false) : setKnowClick(true)
+              }
+            />
+            {knowClick ? (
+              <div className="popBox">
+                <img className="knowPop" src="images/knowImg.png" alt="know" />
+                <FontAwesomeIcon
+                  className="knowX"
+                  icon={faX}
+                  onClick={() => setKnowClick(0)}
+                />
+              </div>
+            ) : (
+              ''
+            )}
           </div>
 
           <ul className="detailList">
@@ -35,25 +88,83 @@ const ProductView = () => {
               <span className="detailClass">판매가</span>
               <span className="detailPrice">
                 ₩
-                {viewData.price
-                  ?.toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                {viewData.option
+                  ? viewData.option[0].price
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                  : ''}
               </span>
             </li>
             <li className="detailText">
-              <span className="detailClass">상품 무게</span> {viewData.weight}
+              <span className="detailClass">상품 무게</span>{' '}
+              {viewData.option ? viewData.option[0].weight : ''}
             </li>
           </ul>
 
           <div className="optionBox">
             <span className="detailClass">용량 </span>
-            <select className="option">
-              <option defaultValue="1">= 옵션 : 가격 =</option>
-              {viewData.option?.map((el, idx) => {
-                return <option key={idx}>{el}</option>;
-              })}
-            </select>
+
+            <div
+              className="select"
+              onClick={() =>
+                clickOptions ? setClickOptions(false) : setClickOptions(true)
+              }
+            >
+              <div className="selectFlex">
+                <div className="selectTitle">= 옵션 : 가격 =</div>
+                <img className="arrow" src="images/arrow.png" alt="arrow" />
+              </div>
+              {clickOptions ? (
+                <ul className="selectList">
+                  <li className="option">= 옵션 : 가격 = </li>
+                  {viewData.option?.map((el, idx) => {
+                    return idx === 0 ? (
+                      <li
+                        name={el.weight}
+                        className="option"
+                        key={el.oid}
+                        onClick={() => {
+                          handleOptions(el);
+                          // handlePrice();
+                        }}
+                      >
+                        {el.weight}
+                      </li>
+                    ) : (
+                      <li
+                        className="option"
+                        key={el.oid}
+                        onClick={() => {
+                          handleOptions(el);
+                          // handlePrice();
+                        }}
+                      >
+                        {el.weight}:{'  '} ₩+
+                        {(el.price - viewData.option[0].price)
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                      </li>
+                    );
+                  })}
+                </ul>
+              ) : (
+                ''
+              )}
+            </div>
           </div>
+          <div className="detailTotal">{totalMap}</div>
+
+          {totalThing.length > 0 ? (
+            <div className="detailPrices">
+              <span className="priceTitle">총제품금액</span>
+              <strong className="tPrice">
+                ₩{tPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              </strong>
+            </div>
+          ) : (
+            ''
+          )}
+
           <div className="detailBtnBox">
             <button className="detailBtn type1">장바구니</button>
             <button className="detailBtn type2">주문하기</button>
@@ -74,7 +185,14 @@ const ProductView = () => {
               배송/교환 및 반품안내
             </a>
           </div>
-          <img src="images/detail1.jpeg" alt="details" />
+
+          <div className="detailContent">
+            <img
+              className="contentImage"
+              src="images/detail1.png"
+              alt="details"
+            />
+          </div>
         </div>
 
         <div id="reviews">
@@ -89,7 +207,14 @@ const ProductView = () => {
               배송/교환 및 반품안내
             </a>
           </div>
-          <img src="images/detail2.jpeg" alt="details" />
+
+          <div className="detailContent">
+            <img
+              className="contentImage"
+              src="images/detail2.png"
+              alt="details"
+            />
+          </div>
         </div>
 
         <div id="delivery">
@@ -104,7 +229,14 @@ const ProductView = () => {
               배송/교환 및 반품안내
             </a>
           </div>
-          <img src="images/detail3.jpeg" alt="details" />
+
+          <div className="detailContent">
+            <img
+              className="contentImage"
+              src="images/detail3.png"
+              alt="details"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -112,9 +244,3 @@ const ProductView = () => {
 };
 
 export default ProductView;
-
-// "id": 1,
-// "name": "드림 크림 셀프-프리저빙",
-// "option": ["45g", "240g", "450g"],
-// "price": 45000,
-// "weight": "450g"
