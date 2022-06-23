3654bd2dd59ebe4c4681402dab40cdf89bac6198
Ahnjungyoung <90163269+Ahnjungyoung@users.noreply.github.com>
2022-06-23 10:34:06 +0900

    로그인페이지 기능구현중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
-
 const Login = () => {
+  const [values, setValues] = useState('');
+  const [pwValues, setPwValues] = useState('');
+  const getValue = e => {
+    setValues(e.target.value);
+  };
+
+  const getPwValue = e => {
+    setPwValues(e.target.value);
+  };
+  console.log(values);
+  console.log(pwValues);
+  //잘모르겠어서 일단씀. 개발중
+
   return (
     <div className="login">
-      <div className="header">
-        <h3>로그인</h3>
-      </div>
-      <div className="membership">
-        <p className="member">회원</p>
-        <p className="nonMember">비회원</p>
-      </div>
+      <div className="header">로그인</div>
       <div className="loginInput">
-        <form>
-          <p>
-            <input className="identification" />
-          </p>
-          <p>
-            <input className="password" />
-          </p>
-          <p>
-            <button className="identificationSave" />
-          </p>
-          <p>
-            <input type="button" className="loginButton" />
-          </p>
-          <footer>
-            <p>회원가입 | 아이디 찾기 | 비밀번호 찾기 |</p>
-          </footer>
+        <form className="inputForm">
+          <input
+            className="identification"
+            type="text"
+            placeholder="아이디"
+            onChange={getValue}
+          />
+
+          <input
+            className="password"
+            type="password"
+            placeholder="비밀번호"
+            onChange={getPwValue}
+          />
+
+          <div className="identificationSave">
+            <input type="checkbox" />
+            <label>아이디 저장</label>
+          </div>
+          <input className="loginButton" type="button" value="로그인" />
+          <div className="link">회원가입 | 아이디 찾기 | 비밀번호 찾기 |</div>
         </form>
       </div>
     </div>
