41e8201b1051a41cb964815845607a09fb7161dc
woohyerim <hyerim3414@gmail.com>
2022-06-27 14:55:44 +0900

    Modify: 내용 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -97,7 +97,6 @@ const Nav = () => {
               <Link to="/join" className="goToMy">
                 <li
                   className="myList"
-                  onMouseOver={() => setIsMyHovering(true)}
                   onMouseOut={() => setIsMyHovering(false)}
                 >
                   회원가입
