23840fa370ddce82e8e8a2cf3acebd44a37c5b41
정예지 <jeong-yeji@jeong-yejiui-MacBookAir.local>
2022-06-27 18:19:05 +0900

    Modify: 수정완료/추가기능구현중

--- src/pages/ProductList/ProductCard.js
+++ src/pages/ProductList/ProductCard.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './ProductCard.scss';
 
-const ProductListDetails = () => {
+const ProductListDetails = ({ sort }) => {
   const [details, setDetails] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch('/data/detailsData.json', {
@@ -14,56 +16,70 @@ const ProductListDetails = () => {
       });
   }, []);
 
+  const goToDetail = () => {
+    navigate(`/view/${details.id}`);
+  };
+
+  sort === 'lowprice' &&
+    details.sort((a, b) => {
+      if (a.price > b.price) {
+        return 1;
+      }
+      return 0;
+    });
+
+  sort === 'highprice' &&
+    details.sort((a, b) => {
+      if (a.price > b.price) {
+        return -1;
+      }
+      return 0;
+    });
+
   return (
-    <div className="productList">
-      <ul>
-        {details.map(list => {
-          return (
-            <li key={list.id}>
-              <div className="innerBox">
-                <div className="thumb">
-                  <div className="img">
-                    <img src={list.image_url} alt="thumb img" />
-                  </div>
-                  <div className="quickIcons">
-                    <ul>
-                      <li>
-                        <p>
-                          <img src="/images/heart.png" alt="heart" />
-                        </p>
-                      </li>
-                      <li>
-                        <p>
-                          <img src="/images/add-to-cart.png" alt="heart" />
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
+    <ul className="productCard">
+      {details.products?.map(list => {
+        return (
+          <li className="productListItem" key={list.id}>
+            <div className="innerBox" onClick={goToDetail}>
+              <div className="thumb">
+                <div className="img">
+                  <img src={list.image_url} alt="thumb" />
                 </div>
-                <div className="icons">
-                  {!!list.is_new && <span className="iconNew">NEW</span>}
-                  {!!list.is_vegan && <span className="iconVegan">VEGAN</span>}
-                  {!!list.is_only_online && (
-                    <span className="iconOnline">온라인전용</span>
-                  )}
-                  {!!list.is_made_in_korea && (
-                    <span className="iconLocal">국내제조</span>
-                  )}
-                  {!!list.is_sold_out && (
-                    <span className="iconSoldOut">품절</span>
-                  )}
+                <div>
+                  <ul className="quickIcons">
+                    <li>
+                      <img src="/images/heart.png" alt="heart" />
+                    </li>
+                    <li>
+                      <img src="/images/add-to-cart.png" alt="heart" />
+                    </li>
+                  </ul>
                 </div>
-                <div className="title">
-                  <h4>{list.name}</h4>
-                  <span className="shotdesc">{list.shotdesc}</span>
-                </div>
-                <div className="price">￦ {list.price}</div>
               </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+              <div className="icons">
+                {!!list.is_new && <span className="iconNew">NEW</span>}
+                {!!list.is_vegan && <span className="iconVegan">VEGAN</span>}
+                {!!list.is_only_online && (
+                  <span className="iconOnline">온라인전용</span>
+                )}
+                {!!list.is_made_in_korea && (
+                  <span className="iconLocal">국내제조</span>
+                )}
+                {!!list.is_sold_out && (
+                  <span className="iconSoldOut">품절</span>
+                )}
+              </div>
+              <div className="title">
+                <h4>{list.name}</h4>
+                <span className="shotdesc">{list.shotdesc}</span>
+              </div>
+              <div className="price">￦ {list.price}</div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import ProductCard from './ProductCard';
 import './ProductList.scss';
 
 const ProductList = () => {
   const [categoryList, setCategoryList] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams(); //이부분은 구현중입니다 :)
+  const navigate = useNavigate();
+
+  const [sort, setSort] = useState();
 
   useEffect(() => {
     fetch('/data/listData.json', {
@@ -11,67 +16,60 @@ const ProductList = () => {
     })
       .then(res => res.json())
       .then(data => {
-        setCategoryList(data);
+        console.log('111', data);
+        setCategoryList(data[0]);
       });
   }, []);
 
-  console.log(categoryList);
+  const goToMenu = () => {
+    navigate(`/list${categoryList.sub_categories.id}`); //이부분도 구현중입니다 :)
+  };
 
   return (
     <>
       <section className="subVisual">
         <div className="img">
-          <img src="" alt="sub visual" />
+          <img src={categoryList.image_url} alt="sub visual" />
         </div>
         <div className="svTitle">
           <div className="innerText">
-            <h2>{categoryList.sub_category_name}</h2>
+            <h2>{categoryList.name}</h2>
             <p>{categoryList.content}</p>
           </div>
         </div>
       </section>
       <section className="subContent">
         <div className="subTitle">
-          <h3>{categoryList.sub_category_name}</h3>
+          <h3>{categoryList.name}</h3>
           <div className="sort">
-            <select>
-              <option value="추천순" key="">
-                추천순
-              </option>
-              <option value="판매인기순" key="">
-                판매인기순
-              </option>
-              <option value="낮은가격순" key="">
-                낮은가격순
-              </option>
-              <option value="높은가격순" key="">
-                높은가격순
-              </option>
-              <option value="리뷰많은순" key="">
-                리뷰많은순
-              </option>
-              <option value="신제품순" key="">
-                신제품순
-              </option>
+            <select
+              onChange={e => {
+                setSort(e.target.value);
+              }}
+            >
+              <option value="recommend">추천순</option>
+              <option value="popularity">판매인기순</option>
+              <option value="lowprice">낮은가격순</option>
+              <option value="highprice">높은가격순</option>
+              <option value="newproduct">신제품순</option>
             </select>
           </div>
         </div>
         <div className="subMenu">
           <ul>
-            {categoryList.sub_categories &&
-              categoryList.sub_categories.map(submenu => {
-                return (
-                  <li key={submenu.id}>
-                    <p>
-                      {submenu.name}
-                      <span>({submenu.product_count})</span>
-                    </p>
-                  </li>
-                );
-              })}
+            {categoryList.sub_categories?.map(submenu => {
+              return (
+                <li key={submenu.id} onClick={goToMenu}>
+                  <p>
+                    {submenu.name}
+                    <span>({submenu.product_count})</span>
+                  </p>
+                </li>
+              );
+            })}
           </ul>
         </div>
-        <ProductCard />
+        <ProductCard sort={sort} />
       </section>
     </>
   );
