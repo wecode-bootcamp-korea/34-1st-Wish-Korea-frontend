eb7752851c2de295bf58848ed95ecabd02f0783b
Sangwon Ha <305243@naver.com>
2022-06-29 16:32:36 +0900

    상세페이지 구현 완료

--- src/pages/ProductView/ProductView.js
+++ src/pages/ProductView/ProductView.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
@@ -14,11 +14,14 @@ const ProductView = () => {
   const [isKnowClick, setIsKnowClick] = useState(false);
   const [totalList, setTotalList] = useState([]);
   const [thumbnailImage, setThumbnailImage] = useState();
-  const navigate = useNavigate();
+  const params = useParams();
   let tPrice = 0;
 
+  // 데이터 받아올 때 fetch
   useEffect(() => {
-    fetch('./data/productDetail.json', { method: 'GET' })
+    fetch(`http://10.58.2.87:8000/products/${params.id}`, {
+      method: 'GET',
+    })
       .then(res => res.json())
       .then(data => {
         data.result.items.length === 1 &&
@@ -28,15 +31,6 @@ const ProductView = () => {
       });
   }, []);
 
-  // 데이터 받아올 때 fetch
-  // useEffect(() => {
-  //   fetch('http://10.58.5.14:8000/products/12', { method: 'GET' })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setViewData(data.result);
-  //     });
-  // }, []);
-
   // 옵션 유무 boolean
   const isOption = viewData.items && viewData.items.length !== 1;
 
@@ -44,21 +38,25 @@ const ProductView = () => {
   const handleOptions = el => {
     const selectArr = []; //현제 들어온 데이터들의 옵션 아이디를 배열에 저장
     totalList.map(value => selectArr.push(value.id)); //이번에 들어온 옵션아이디가 없으면 추가, 수량 추가
-    if (!selectArr.includes(el.id)) {
+    if (!selectArr.includes(el.id) && el.stock !== 0) {
       setTotalList([...totalList, el]);
-      // el.id = viewData.product_id;
       el.quantity = 1; // 수량 칼럼 추가
-    } else alert('이미 선택된 옵션입니다.');
+    } else if (selectArr.includes(el.id)) alert('이미 선택된 옵션입니다.');
+    else if (el.stock === 0) alert('재고가 없습니다');
+    else alert('에러');
   };
 
   totalList.sort((a, b) => a.id - b.id); //옵션 정렬
 
   //옵션 증가
   const increaseOption = id => {
-    setTotalList(val =>
-      val.map(el => {
+    setTotalList(list =>
+      list.map(el => {
         // 클릭 이벤트로 받아온 아이디와 저장한 리스트의 아이디를 비교하여 값 변환
-        if (id === el.id) el.quantity++; //재고 시스템 도입시 여기에 조건 걸어주면 됨
+        if (id === el.id && el.quantity < el.stock)
+          el.quantity++; //재고 시스템 도입시 여기에 조건 걸어주면 됨
+        else if (id === el.id && el.quantity >= el.stock)
+          alert(`재고 부족 / 현재 재고 : ${el.stock}개`);
         return el;
       })
     );
@@ -66,8 +64,8 @@ const ProductView = () => {
 
   //옵션 감소
   const decreaseOption = id => {
-    setTotalList(val =>
-      val.map(el => {
+    setTotalList(list =>
+      list.map(el => {
         if (id === el.id && el.quantity > 1) el.quantity--;
         return el;
       })
@@ -82,10 +80,27 @@ const ProductView = () => {
   };
 
   const goToCart = () => {
-    navigate('/view');
-    alert('미구현');
+    fetch('http://10.58.2.87:8000/orders/carts', {
+      method: 'POST',
+      headers: { Authorization: localStorage.getItem('Authorization') },
+      body: JSON.stringify({
+        items: totalList,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.message === 'Out of stock') {
+          alert('재고가 부족합니다');
+        } else {
+          if (totalList.length !== 0) {
+            alert('장바구니에 담았습니다');
+          } else alert('옵션을 선택해주세요');
+        }
+      });
+    if (isOption) {
+      setTotalList([]);
+    }
   };
-
   // console.log('백엔드에서받은거:', viewData, '장바구니 보내줄거:', totalList);
   return (
     <div className="productView">
@@ -121,15 +136,15 @@ const ProductView = () => {
                 </span>
               );
             })}
-            {/* 수정중~!~!~!~! */}
             <img
               className="detailShare"
               src="/images/view/share.png"
               alt="share"
               onClick={e => {
                 e.preventDefault();
-                // navigator.clipboard.writeText(window.location.href);
-                alert(window.location.href);
+                let url = window.location.href;
+                navigator.clipboard.writeText(url);
+                alert('복사 완료');
               }}
             />
             <img
@@ -141,8 +156,7 @@ const ProductView = () => {
           </span>
           <div className="detailKeyword">{viewData.tag}</div>
           <div className="detailText">
-            Good to know
-            {'  '}
+            Good to know&nbsp;
             <img
               className="know"
               src="/images/view/know.png"
@@ -211,12 +225,43 @@ const ProductView = () => {
           )}
           {/* 버튼 */}
           <div className="detailBtnBox">
-            <button className="detailBtn type1" onClick={() => goToCart()}>
-              장바구니
-            </button>
-            <button className="detailBtn type2" onClick={() => alert('미구현')}>
-              주문하기
-            </button>
+            {isOption ? (
+              <>
+                <button className="detailBtn type1" onClick={() => goToCart()}>
+                  장바구니
+                </button>
+                <button
+                  className="detailBtn type2"
+                  onClick={() => alert('장바구니를 이용해주세요')}
+                >
+                  주문하기
+                </button>
+              </>
+            ) : (
+              <>
+                {totalList[0]?.stock === 0 ? (
+                  <button
+                    className="detailBtn type3"
+                    onClick={() => alert('품절입니다')}
+                  >
+                    품절
+                  </button>
+                ) : (
+                  <button
+                    className="detailBtn type1"
+                    onClick={() => goToCart()}
+                  >
+                    장바구니
+                  </button>
+                )}
+                <button
+                  className="detailBtn type2"
+                  onClick={() => alert('미구현')}
+                >
+                  주문하기
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
